198b1a0696dc77383b2dd155a9359f029d294d15
AGROVANTS <55609306+AGROVANTS-AGTECH@users.noreply.github.com>
2020-06-09 02:48:56 -0300

    Add files via upload

---
+++ data/MZPULVERIZACIONJUNIO2020_1.js
@@ -0,0 +1,9 @@
+var json_MZPULVERIZACIONJUNIO2020_1 = {
+"type": "FeatureCollection",
+"name": "MZPULVERIZACIONJUNIO2020_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "FECHA": "2020-06-02", "HECTAREAS": 0.7, "AGROQUIMIC": "POST-EMERGENTE\nHebadox 3000\nBromotril 2500\nSherman 200\nEstimulate 250", "LTSxHAS": 10.0, "CULTIVO": "ajo", "HERBADOX": 3.0, "LINURON": 0.0, "BROMOTRIL": 2.5, "SHERMAN PL": 0.2, "H2O": 5.5, "TOT-HAS": 2.4, "ESTIMULATE": 0.25, "CAMPO_": "RUTA 88" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -69.078729765947145, -33.389215993005919 ], [ -69.078340801323819, -33.389046066300445 ], [ -69.077399836551166, -33.390716477465318 ], [ -69.077818371928316, -33.390865116932488 ], [ -69.078729765947145, -33.389215993005919 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": "2", "FECHA": "2020-06-02", "HECTAREAS": 1.7, "AGROQUIMIC": "PRE-EMERGENTE\nLinuron 1000\nHebadox 3000", "LTSxHAS": 10.0, "CULTIVO": "ajo", "HERBADOX": 3.0, "LINURON": 1.5, "BROMOTRIL": 0.0, "SHERMAN PL": 0.0, "H2O": 4.0, "TOT-HAS": 2.4, "ESTIMULATE": 0.0, "CAMPO_": "RUTA 88" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -69.073573046551971, -33.387309163112157 ], [ -69.072675296515825, -33.388965255344523 ], [ -69.07352132806119, -33.389233226732649 ], [ -69.074380998706772, -33.387615504109966 ], [ -69.073573046551971, -33.387309163112157 ] ] ] ] } }
+]
+}
